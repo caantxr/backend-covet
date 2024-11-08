318540101ef012138d65700eef118a6b4ecdb36e
caantxr <miguelcantorm@gmail.com>
2024-11-07 20:17:12 -0500

    revision2

---
+++ src/controllers/reservations.js
@@ -0,0 +1,69 @@
+const { dbGetReservations, dbGetReservationById, dbInsertReservation, dbUpdateReservation, dbDeleteReservation } = require("../services/reservation");
+
+// Controlador para obtener todas las reservas
+const getReservations = async (req, res) => {
+    try {
+        const reservations = await dbGetReservations();
+        res.status(200).json(reservations);
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener las reservas." });
+    }
+};
+
+// Controlador para obtener una reserva por ID
+const getReservationById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const reservation = await dbGetReservationById(id);
+        if (!reservation) return res.status(404).json({ error: "Reserva no encontrada." });
+        res.status(200).json(reservation);
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener la reserva." });
+    }
+};
+
+// Controlador para crear una nueva reserva
+const createReservation = async (req, res) => {
+    const { userId, eventId, numberOfTickets, reservationDate, status } = req.body;
+    try {
+        const newReservation = { userId, eventId, numberOfTickets, reservationDate, status };
+        const reservation = await dbInsertReservation(newReservation);
+        res.status(201).json(reservation);
+    } catch (error) {
+        res.status(500).json({ error: "Error al crear la reserva." });
+    }
+};
+
+// Controlador para actualizar una reserva existente
+const updateReservation = async (req, res) => {
+    const { id } = req.params;
+    const { userId, eventId, numberOfTickets, reservationDate, status } = req.body;
+    try {
+        const updatedReservation = { userId, eventId, numberOfTickets, reservationDate, status };
+        const reservation = await dbUpdateReservation(id, updatedReservation);
+        if (!reservation) return res.status(404).json({ error: "Reserva no encontrada." });
+        res.status(200).json(reservation);
+    } catch (error) {
+        res.status(500).json({ error: "Error al actualizar la reserva." });
+    }
+};
+
+// Controlador para eliminar una reserva
+const deleteReservation = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const reservation = await dbDeleteReservation(id);
+        if (!reservation) return res.status(404).json({ error: "Reserva no encontrada." });
+        res.status(200).json({ message: "Reserva eliminada con éxito." });
+    } catch (error) {
+        res.status(500).json({ error: "Error al eliminar la reserva." });
+    }
+};
+
+module.exports = {
+    getReservations,
+    getReservationById,
+    createReservation,
+    updateReservation,
+    deleteReservation
+};
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -7,6 +7,8 @@ const {
     updateCategory,
     deleteCategory
 } = require('../controllers/categories');
+const { authRole } = require('../middlewares/auth-role.middleware');
+const { authUser } = require('../middlewares/auth-user.middleware');
 
 // Obtener todas las categorías
 // Respuesta: Array de categorías
@@ -19,15 +21,15 @@ router.get('/:id', getCategoryById);
 // Crear nueva categoría
 // Requiere: { name, description }
 // Respuesta: Objeto de la nueva categoría creada
-router.post('/', createCategory); 
+router.post('/', authUser, authRole, createCategory); 
 
 // Actualizar categoría
 // Requiere: { name, description } (solo los campos que se deseen actualizar)
 // Respuesta: Objeto de la categoría actualizada
-router.patch('/:id', updateCategory); 
+router.patch('/:id', authUser, authRole, updateCategory); 
 
 // Eliminar categoría
 // Respuesta: Mensaje de éxito o error
-router.delete('/:id', deleteCategory); 
+router.delete('/:id', authUser, authRole, deleteCategory); 
 
 module.exports = router;
